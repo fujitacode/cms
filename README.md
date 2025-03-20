40719f8ecf496f518343c59260f09f1880b45a40
fujitacode <riot.jack.bbb@gmail.com>
2025-03-20 19:52:57 +0900

    ✨feature: READMEの更新とバックエンドにserdeおよびserde_jsonを追加、APIエンドポイントを実装

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-frontend
+frontend <br>
 next.js version 15.2.3
 
-backend 
+backend <br>
 web-actix version 4.10.2
+
+自作CMS作りたいという願望
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct HelloResponse {
+    message: String,
+}
+
+#[get("/api/hello")]
+async fn hello_world() -> impl Responder {
+    let response = HelloResponse {
+        message: String::from("Hello, World!"),
+    };
+
+    HttpResponse::Ok().json(response)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server at http://localhost:8080");
+
+    HttpServer::new(|| App::new().service(hello_world))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
